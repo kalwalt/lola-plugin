b289f5cd93ec2454f766c36787331de6d6d4fad0
kalwalt <info@kalwaltart.it>
2019-01-20 22:28:41 +0100

    moving Translator class outside of Lola class

--- lib/lola.rb
+++ lib/lola.rb
@@ -5,17 +5,4 @@ class Lola
   end
 end
 
-class Lola::Translator
-  def initialize(language)
-    @language = language
-  end
-
-  def hi
-    case @language
-    when "spanish"
-      "hola mundo"
-    else
-      "hello world"
-    end
-  end
-end
+require 'hola/translator'
